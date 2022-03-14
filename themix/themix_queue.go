a93c7f6f6f9a2362e26bec6586881bca8c805c79
Snowfall99 <445117445@qq.com>
2022-03-14 21:53:01 +0800

    themix: themix queu init and run

--- themix/themix_queue.go
+++ themix/themix_queue.go
@@ -1,7 +1,35 @@
 package themix
 
+import (
+	"log"
+
+	"themix.new.io/common/messagepb"
+)
+
 type ThemixQue struct {
-	// TODO(chenzx): To be implemented.
-	msgc  map[uint64]*Msg
-	queue map[uint64]*Themix
+	inputc  chan *messagepb.Msg
+	outputc chan *messagepb.Msg
+	repc    chan []byte
+	msgc    map[uint64]*messagepb.Msg
+	queue   map[uint64]*Themix
+}
+
+func initThemixQue(inputc, outputc chan *messagepb.Msg, repc chan []byte) *ThemixQue {
+	return &ThemixQue{
+		inputc:  inputc,
+		outputc: outputc,
+		repc:    repc,
+		msgc:    make(map[uint64]*messagepb.Msg),
+		queue:   make(map[uint64]*Themix),
+	}
+}
+
+func (themixQue *ThemixQue) run() {
+	// TODO(chenzx): A thread pool to get msg from input channel and handle them.
+	// This is supposed to be implemented as a for loop.
+	for {
+		<-themixQue.inputc
+		themixQue.repc <- []byte{}
+		log.Println("reply to client")
+	}
 }
