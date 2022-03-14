68c53c06ea11f78d25d5eb99bd93887bce9967a1
Snowfall99 <445117445@qq.com>
2022-03-14 21:53:18 +0800

    themix: node init and run

--- themix/node.go
+++ themix/node.go
@@ -1,9 +1,36 @@
 package themix
 
+import (
+	"themix.new.io/client/clientpb"
+	"themix.new.io/common/messagepb"
+	"themix.new.io/config/configpb"
+	"themix.new.io/noise"
+)
+
 type Node struct {
-	inputc    chan *Msg
-	outputc   chan *Msg
-	reqc      chan *Request
-	repc      chan *Response
-	themixQue ThemixQue
+	// proposer  *Proposer
+	themixQue *ThemixQue
+}
+
+func InitNode(id, batch uint32, peers []*configpb.Peer) *Node {
+	inputc := make(chan *messagepb.Msg)
+	outputc := make(chan *messagepb.Msg)
+	reqc := make(chan *clientpb.Request)
+	repc := make(chan []byte)
+	client := peers[id].Client
+	go initProposer(batch, client, reqc, repc, outputc)
+	themixQue := initThemixQue(inputc, outputc, repc)
+	peerInfo := make(map[uint32]string)
+	for _, peer := range peers {
+		peerInfo[peer.Id] = peer.Addr
+	}
+	noise.InitNoise(id, peerInfo, inputc, outputc)
+	return &Node{
+		themixQue: themixQue,
+	}
+}
+
+func (node *Node) Run() {
+	// go node.proposer.run()
+	node.themixQue.run()
 }
