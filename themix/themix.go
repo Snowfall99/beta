27785ee5918e611bb4472e9779ec8762a77a30d9
Snowfall99 <445117445@qq.com>
2022-03-21 16:10:49 +0800

    themix: themix init and run

--- themix/themix.go
+++ themix/themix.go
@@ -1,5 +1,39 @@
 package themix
 
+import (
+	"log"
+
+	"themix.new.io/common/messagepb"
+)
+
 type Themix struct {
 	// TODO(chenzx): To be implemented.
+	inputc  chan *messagepb.Msg
+	msgc    map[uint32]chan *messagepb.Msg
+	repc    chan []byte
+	n       uint32
+	decided uint32
+}
+
+func initThemix(inputc chan *messagepb.Msg, repc chan []byte, n uint32) *Themix {
+	themix := &Themix{
+		inputc: inputc,
+		repc:   repc,
+		msgc:   make(map[uint32]chan *messagepb.Msg),
+		n:      n,
+	}
+	for i := 0; i < int(n); i++ {
+		msgc := make(chan *messagepb.Msg)
+		themix.msgc[uint32(i)] = msgc
+		initInstance(msgc, n)
+	}
+	return themix
+}
+
+func (themix *Themix) run() {
+	for {
+		msg := <-themix.inputc
+		log.Println("msg: ", msg)
+		themix.msgc[msg.Proposer] <- msg
+	}
 }
