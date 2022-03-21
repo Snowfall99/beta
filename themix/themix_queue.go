21542ec1e7550d000f726752ee84c3eb22187394
Snowfall99 <445117445@qq.com>
2022-03-21 16:14:48 +0800

    themix queue: themix queue init and run

--- themix/themix_queue.go
+++ themix/themix_queue.go
@@ -1,8 +1,6 @@
 package themix
 
 import (
-	"log"
-
 	"themix.new.io/common/messagepb"
 )
 
@@ -10,17 +8,17 @@ type ThemixQue struct {
 	inputc  chan *messagepb.Msg
 	outputc chan *messagepb.Msg
 	repc    chan []byte
-	msgc    map[uint64]*messagepb.Msg
-	queue   map[uint64]*Themix
+	msgc    map[uint32]chan *messagepb.Msg
+	n       uint32
 }
 
-func initThemixQue(inputc, outputc chan *messagepb.Msg, repc chan []byte) *ThemixQue {
+func initThemixQue(inputc, outputc chan *messagepb.Msg, repc chan []byte, n uint32) *ThemixQue {
 	return &ThemixQue{
 		inputc:  inputc,
 		outputc: outputc,
 		repc:    repc,
-		msgc:    make(map[uint64]*messagepb.Msg),
-		queue:   make(map[uint64]*Themix),
+		msgc:    make(map[uint32]chan *messagepb.Msg),
+		n:       n,
 	}
 }
 
@@ -28,8 +26,18 @@ func (themixQue *ThemixQue) run() {
 	// TODO(chenzx): A thread pool to get msg from input channel and handle them.
 	// This is supposed to be implemented as a for loop.
 	for {
-		<-themixQue.inputc
-		themixQue.repc <- []byte{}
-		log.Println("reply to client")
+		// Get msg from input channel.
+		// Route msg to the right themix instance according to seq.
+		// If related instance doesn't exist, create it.
+		msg := <-themixQue.inputc
+		if themixQue.msgc[msg.Seq] != nil {
+			themixQue.msgc[msg.Seq] <- msg
+		} else {
+			ch := make(chan *messagepb.Msg)
+			themixQue.msgc[msg.Seq] = ch
+			themix := initThemix(ch, themixQue.repc, themixQue.n)
+			go themix.run()
+			themixQue.msgc[msg.Seq] <- msg
+		}
 	}
 }
