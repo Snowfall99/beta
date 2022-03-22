b531bc46f39d1d6bdef0b2fcf50afe2bd1e5c85a
Snowfall99 <445117445@qq.com>
2022-03-22 12:29:45 +0800

    main: add initLog

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"log"
 	"os"
 
 	"google.golang.org/protobuf/encoding/prototext"
@@ -9,11 +11,20 @@ import (
 	"themix.new.io/themix"
 )
 
+func initLog(id int) {
+	file, err := os.OpenFile(fmt.Sprintf("../log/%d.log", id), os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0600)
+	if err != nil {
+		panic(err)
+	}
+	log.SetOutput(file)
+}
+
 func main() {
 	// TODO(chenzx): Under construction.
 	// id is set as a flag parameter for local test
 	id := flag.Uint("id", 0, "ID of themix node")
 	flag.Parse()
+	initLog(int(*id))
 	data, err := os.ReadFile("themix.config")
 	if err != nil {
 		panic(err)
@@ -23,6 +34,6 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	node := themix.InitNode(uint32(*id), configuration.Batch, configuration.N, configuration.Peers)
+	node := themix.InitNode(uint32(*id), int(configuration.Batch), int(configuration.N), int(configuration.F), configuration.Peers)
 	node.Run()
 }
