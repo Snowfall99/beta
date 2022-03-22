f4e7deea1d533ce33feb05864c5bc40aeb3e3465
Snowfall99 <445117445@qq.com>
2022-03-22 12:06:29 +0800

    themix: fulfill themix init and run
    themix: add themix field: proposed, id, f, outputc, reqc
    
    run: if decided num == 1 and have not proposed any proposal, then, propose an empty proposal to make the system forward. If decided num == n then release reqc and repc(resource that will be used by other instances or themix), and break the for loop to enter garbage collection stage.

--- themix/themix.go
+++ themix/themix.go
@@ -3,37 +3,73 @@ package themix
 import (
 	"log"
 
+	"themix.new.io/client/clientpb"
 	"themix.new.io/common/messagepb"
 )
 
 type Themix struct {
 	// TODO(chenzx): To be implemented.
-	inputc  chan *messagepb.Msg
-	msgc    map[uint32]chan *messagepb.Msg
-	repc    chan []byte
-	n       uint32
-	decided uint32
+	inputc   chan *messagepb.Msg
+	outputc  chan *messagepb.Msg
+	msgc     map[uint32]chan *messagepb.Msg
+	reqc     chan *clientpb.Request
+	repc     chan []byte
+	decideCh chan []byte
+	id       uint32
+	n        int
+	f        int
+	decided  int
+	proposed bool
 }
 
-func initThemix(inputc chan *messagepb.Msg, repc chan []byte, n uint32) *Themix {
+func initThemix(inputc chan *messagepb.Msg, outputc chan *messagepb.Msg, reqc chan *clientpb.Request, repc chan []byte, n, f int, id uint32) *Themix {
+	decideCh := make(chan []byte)
 	themix := &Themix{
-		inputc: inputc,
-		repc:   repc,
-		msgc:   make(map[uint32]chan *messagepb.Msg),
-		n:      n,
+		inputc:   inputc,
+		outputc:  outputc,
+		reqc:     reqc,
+		repc:     repc,
+		decideCh: decideCh,
+		msgc:     make(map[uint32]chan *messagepb.Msg),
+		id:       id,
+		n:        n,
+		f:        f,
 	}
 	for i := 0; i < int(n); i++ {
 		msgc := make(chan *messagepb.Msg)
 		themix.msgc[uint32(i)] = msgc
-		initInstance(msgc, n)
+		go initInstance(msgc, outputc, decideCh, n, f, uint32(i))
 	}
 	return themix
 }
 
 func (themix *Themix) run() {
+	go func() {
+		for {
+			<-themix.decideCh
+			themix.decided++
+			log.Println("themix decide number: ", themix.decided)
+			if themix.decided == 1 {
+				if !themix.proposed {
+					themix.reqc <- &clientpb.Request{}
+				}
+			} else if themix.decided == themix.n {
+				themix.repc <- []byte{}
+				themix.reqc = nil
+				themix.repc = nil
+				break
+			}
+		}
+	}()
 	for {
 		msg := <-themix.inputc
-		log.Println("msg: ", msg)
+		if msg.Type == messagepb.MsgType_VAL && msg.Proposer == themix.id {
+			themix.proposed = true
+		}
+		// TODO(chenzx): this thread should exit after decide listen thread
+		// if themix.decided == themix.n {
+		// 	break
+		// }
 		themix.msgc[msg.Proposer] <- msg
 	}
 }
